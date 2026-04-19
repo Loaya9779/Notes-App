b34e84f0784fe42cf8238e8cafbdfc158544beb6
Loaya9779 <loayadel96@gmail.com>
2026-04-19 22:52:00 +0200

    update project structure and change theme

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const NotesApp());
+}
+
+class NotesApp extends StatelessWidget {
+  const NotesApp
+({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Notes App',
+      theme: ThemeData(
+        brightness: Brightness.dark,
+      ),
+      home: const Scaffold(
+        body: Center(
+          child: Text('Notes App'),
+        ),
+      ),
+    );
+  }
+}
